11b4882014a54c6e861ee7c30ba788a469f918de
GreedShadow <markquimora73@gmail.com>
2026-05-21 20:58:38 +0800

    Fallback to demo API without database env

--- api/[...path].js
+++ api/[...path].js
@@ -1,3 +1,10 @@
 import app from '../server/src/app.js';
+import demoHandler from '../client/api/[...path].js';
 
-export default app;
+export default function handler(req, res) {
+  if (process.env.VERCEL && !process.env.DB_HOST) {
+    return demoHandler(req, res);
+  }
+
+  return app(req, res);
+}
